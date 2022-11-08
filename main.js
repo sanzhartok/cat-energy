091b10cf13b21cdd9358728cbc36a16d6b87fea6
Beksultan <beksultan.sagnaev@gmail.com>
2022-11-08 11:06:54 +0600

    init: first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function beforeAfter() {  
+    document.getElementById('kobavenusab').style.width = document.getElementById('pedsumid').value + "%";
+    console.log(document.getElementById('pedsumid').value);
+  }
+
+function hello() {
+    alert("hello Sanzhar!")
+}
