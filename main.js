73b3c0af176ec194c0f92618ce29ea67618b87fb
sanzhar <sanzhar.toksharinov@gmail.com>
2022-12-15 09:31:00 +0600

    init: add cat energy

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-function beforeAfter() {  
-    document.getElementById('kobavenusab').style.width = document.getElementById('pedsumid').value + "%";
+function beforeAfter() {
+    document.getElementsByClassName('slider__after')[0].style.width = document.getElementById('pedsumid').value + "%";
+    document.getElementsByClassName('slider__before')[0].style.width = (100 - document.getElementById('pedsumid').value) + "%";
     console.log(document.getElementById('pedsumid').value);
-  }
-
-function hello() {
-    alert("hello Sanzhar!")
 }
